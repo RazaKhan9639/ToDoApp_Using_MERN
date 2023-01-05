43f135c0c2728f3cc64a74980ba98f7116454cca
RazaKhan <raza.khan4336@gmail.com>
2023-01-05 15:25:31 +0000

    Try Simple Changes

--- server/index.js
+++ server/index.js
@@ -1,26 +1,18 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import cors from "cors";
-import todosRoutes from "./routes/todos.js";
-
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import todosRoutes from './routes/todos.js';
 const app = express();
 dotenv.config();
-app.use(express.json({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(cors());
-app.use("/todos", todosRoutes);
-mongoose.set("strictQuery", false);
-app.get("/", (req, res) => {
-  res.send("hello welcome to Todo App Server");
-});
+app.use('/todos', todosRoutes)
+
+app.get('/', (req, res) => {
+    res.send('Welcome to server')
+})
 const PORT = process.env.PORT || 5000;
-mongoose
-  .connect(process.env.mongodb, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-  )
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.mongodb, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => app.listen(PORT, () => console.log(`server is running on port ${PORT}`))).catch(err => console.log(err))
+
